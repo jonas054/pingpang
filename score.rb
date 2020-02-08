33d8bfae43882fa4d4758942a0feb905a3ac1387
Jonas Arvidsson <jonas054@gmail.com>
2020-02-08 07:53:27 +0100

    Move scores from the edges to the middle

--- score.rb
+++ score.rb
@@ -2,9 +2,11 @@
 
 # Keeping the scores and displaying them.
 class Score
+  FONT_SIZE = 40
+
   def initialize(window)
     @left_score = @right_score = 0
-    @font = Gosu::Font.new(window, 'Arial', 40)
+    @font = Gosu::Font.new(window, 'Arial', FONT_SIZE)
     @window = window
   end
 
@@ -21,9 +23,11 @@ class Score
   end
 
   def display
-    @font.draw_text(@left_score.to_s, 10, 10, 0, 1, 1, Gosu::Color::GREEN)
-    @font.draw_text(@right_score.to_s,
-                    @window.width - 20 - 14 * @right_score.to_s.length, 10, 0,
-                    1, 1, Gosu::Color::GREEN)
+    middle = @window.width / 2
+    left = @left_score.to_s
+    right = @right_score.to_s
+    @font.draw_text(left, middle - 20 - left.length* FONT_SIZE / 2.3, 10,
+                    0, 1, 1, Gosu::Color::GREEN)
+    @font.draw_text(right, middle + 15, 10, 0, 1, 1, Gosu::Color::GREEN)
   end
 end
