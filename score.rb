7f7e4a6319ec1d840a7ebb4a995f7378a13cc159
Jonas Arvidsson <jonas054@gmail.com>
2020-02-08 09:15:28 +0100

    Refactor Score#display()

--- score.rb
+++ score.rb
@@ -3,6 +3,7 @@
 # Keeping the scores and displaying them.
 class Score
   FONT_SIZE = 40
+  FONT_WIDTH = FONT_SIZE / 2.3
 
   def initialize(window)
     @left_score = @right_score = 0
@@ -25,9 +26,13 @@ class Score
   def display
     middle = @window.width / 2
     left = @left_score.to_s
-    right = @right_score.to_s
-    @font.draw_text(left, middle - 20 - left.length* FONT_SIZE / 2.3, 10,
-                    0, 1, 1, Gosu::Color::GREEN)
-    @font.draw_text(right, middle + 15, 10, 0, 1, 1, Gosu::Color::GREEN)
+    draw_text(left, middle - (1 + left.length) * FONT_WIDTH, 10)
+    draw_text(@right_score, middle + FONT_WIDTH, 10)
+  end
+
+  private
+
+  def draw_text(text, x_pos, y_pos)
+    @font.draw_text(text, x_pos, y_pos, 0, 1, 1, Gosu::Color::GREEN)
   end
 end
