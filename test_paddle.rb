e6d15ea34a1e3e28a4d6e211f93db5ab9ff9a9c1
Jonas Arvidsson <jonas054@gmail.com>
2020-03-13 21:53:37 +0100

    Add testing for Paddle class

---
+++ test_paddle.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+require './paddle'
+require './score'
+
+class TestPaddle < Test::Unit::TestCase
+  def setup
+    @score = Score.new(Gosu::Window.new(100, 100))
+    @paddle = Paddle.new(@score)
+  end
+
+  def test_moving
+    @paddle.place(3, 5)
+    @paddle.set_off(0, 1)
+    @paddle.move
+
+    assert_equal 6, @paddle.total_speed
+    assert_equal 3, @paddle.x
+    assert_equal 11, @paddle.y
+  end
+
+  def test_stop
+    @paddle.place(3, 5)
+    @paddle.stop
+    @paddle.move
+
+    assert_equal 3, @paddle.x
+    assert_equal 5, @paddle.y
+  end
+
+  def test_total_speed
+    assert_equal 6, @paddle.total_speed
+    10.times do
+      @score.increment(:left)
+      @score.increment(:right)
+    end
+    assert_equal 8, @paddle.total_speed
+  end
+end
